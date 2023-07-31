c722bddc8f8c21aa13e3b6845818bc8967b159f7
YassineEddyb <yassineeddyb@gmail.com>
2023-07-31 09:52:16 +0100

    Solved: Rotate Array

--- rotateArray.cpp
+++ rotateArray.cpp
@@ -6,49 +6,55 @@ using namespace std;
 /*
     - Problem: Rotate Array
 
-    - Difficulty:
+    - Difficulty: Meduim
 
 	- Subject: 
+        Given an integer array nums, rotate the array to the right 
+        by k steps, where k is non-negative.
 		
 	- Example: 
         Input: nums = [1,2,3,4,5,6,7], k = 3
         Output: [5,6,7,1,2,3,4]
 		
-	- Algorithem: 
+	- Algorithem: Reverse Array
 
-	- Time complixity: 
-	- Space complixity:
+	- Time complixity: O(n)
+	- Space complixity: O(1)
 
 	- Approach:
+        first since rotate by k == nums.size() does not change the
+        array we use k = k % nums.size() to avoid getting out of range,
+        After that we reverse the hole array, Then we reverse the 
+        array from 0 to k - 1 and from k to nums.size() -1;
 */
 
-void rotateArray(vector<int>& nums, int k) {
-    vector<int> arr(nums.size()) ;
-    int j = nums.size() - 1;
-
-    if (nums.size() == 1 || k == 0) return;
-    
-    k = k % nums.size();
-
-    for(int i = k ; i >= 0; i--) {
-        nums[j] = nums[i];
-        j--;
-    }
-    
-    for (int i = nums.size() - 1; i > k ; i--) {
-        nums[j] = copy_nums[i];
-        j--;
+void reverse(vector<int>& nums, int start, int end) {
+    int temp;
+    while(end > start) {
+        temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--; 
     }
 }
 
+
+void rotateArray(vector<int>& nums, int k) {
+    k = k % nums.size();
+    
+    reverse(nums, 0, nums.size() - 1);
+    reverse(nums, 0, k  - 1);
+    reverse(nums, k, nums.size() - 1);
+}
+
 int main () {
-    // vector<int> vec = {1,2,3,4,5,6,7};
-    vector<int> vec = {-1,-100};
-    int k = 2;
+    vector<int> vec = {1,2,3,4,5,6,7};
+    int k = 3;
 
 	rotateArray(vec, k);
 
-	cout << "Expected: " << "1 2 3 4 5 6 7" << endl;
+    cout << "Expected: " << "5 6 7 1 2 3 4" << endl;
 
 	cout << "Output:   ";
 	for(int i = 0; i < vec.size(); i++) {
