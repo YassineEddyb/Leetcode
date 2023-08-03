c5049d3b4cc79b5553708f61f4061e6047f66c9f
YassineEddyb <yassineeddyb@gmail.com>
2023-08-03 20:00:07 +0100

    Solved: Jump Game 2

--- jumpGame2.cpp
+++ jumpGame2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
@@ -27,55 +28,81 @@ using namespace std;
 		Input: nums = [2,3,1,1,4]
         Output: 2
 		
-	- Algorithem: gready method
+	- Algorithem: Dynamic Programing, Gready Method
 
-	- Time complixity: O(n)
-	- Space complixity: O(1)
+	- Time complixity: dp O(n^2), gready O(n)
+	- Space complixity: dp O(n), gready O(1)
 
-	- Approach:
-		I used pos to keep truck of the position with most steps
-		in it (gready), On every iteration I check if pos < nums[i]
-		if so then pos = nums[i] and I decrease pos with 1,
-		But first I check if (pos == 0 and nums[i] == 0) than means
-		the we can not move to the next index I return 0, If everythig
-		is ok means that we reached the end of array and I return 1. 
+	- Approach1 dp:
+        I used dynamic programing memoization to traverse
+        all the node and take the min steps tha you can make.
+		
+    - Approach2 gready:
+        I used left (l) and right (r) wich represent the start
+        and the end of a segment to iterate over it, then 
+        I take the max reach that I can make form this segment
+        And I move l to r + 1 and r to the max reach index. every
+        time those steps complete I increment steps which will be
+        returned at the end.
 */
 
-int jump(vector<int>& nums) {
-    int i = 0, pos = nums[0], k = 1, max = 0, maxPos = 0;
 
-    if (nums.size() == 1)
+int minJump(vector<int>& nums, vector<int>& store, int n) {
+    // Base case this means that we reached the end of nums
+    if (n >= nums.size() - 1)
         return 0;
+    
+    // Check if the node is already calculated
+    if (store[n])
+        return store[n];
+    
+    int jumps = INT_MAX;
 
-	while (i < nums.size()) {
-		while(pos > 0) {
-            if (nums[pos] + pos > max) {
-                max = nums[pos] + pos;
-                maxPos = pos;
-            }
-		    pos--;
+    // Loop over all the jump that you can make from this node
+    for (int i = n + 1; i <= n + nums[n]; i++)
+        // take the min between jumps and the result recurtion 
+        jumps = min(jumps, 1 + minJump(nums, store, i));
+
+    // store the min jumps for n
+    store[n] = jumps;
+
+    return store[n];
+}
+
+int jump(vector<int>& nums) {
+    // intailize the store with the length of array nums
+    vector<int> store(nums.size());
+    // call the minJump which will be recursive
+    return minJump(nums, store, 0);
+}
+
+int jump2(vector<int>& nums) {
+    int l = 0, r = 0, steps = 0;
+
+    // Loop over the array nums until (r) reach the last index
+    while(r < nums.size() - 1) {
+        int maxSteps = 0;
+        // Loop from left to the right of this segment
+        while(l <= r) {
+            // Take the max reach you can make from this segment
+            maxSteps = max(maxSteps, l + nums[l]);
+            l++;
         }
-        i = maxPos;
-        pos = nums[maxPos];
-        k++;
-        cout << pos << maxPos << i << endl;
-        // exit(1);
-        if (pos + i > nums.size() - 1) return k;
-	}
+        // Move r to the max reach index and l will be in r position
+        r = maxSteps;
+        // Increment steps;
+        steps++;
+    }
 
-	return k;
+    return steps;
 }
 
 int main () {
-	// vector<int> vec = {7,0,9,6,9,6,1,7,9,0,1,2,9,0,3};
-	// vector<int> vec = {2,3,0,1,4};
-	vector<int> vec = {1,1,1,1};
-	// vector<int> vec = {1, 0};
-	// vector<int> vec = {0};
+	vector<int> vec = {2,3,1,1,4};
 
 	int ret = jump(vec);
-	// int ret2 = jump(vec2);
+	int ret2 = jump2(vec);
 
-    cout << "Expected: 2" << endl;
-	cout << "Output:   " << ret << endl;
+    cout << "Expected: 2 2" << endl;
+	cout << "Output:   " << ret << " " << ret2 << endl;
 }
