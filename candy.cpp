20ab8f474027b3457cc4a4d61ee465345c484526
YassineEddyb <yassineeddyb@gmail.com>
2023-08-06 18:54:27 +0100

    Solved: Candy

--- candy.cpp
+++ candy.cpp
@@ -34,28 +34,38 @@ using namespace std;
 	- Space complixity: O(n)
 
 	- Approach:
-        
+        I used an array candies to keep truck of the number of
+        candies of each child, first we iterate through ratings
+        from lefe to right and calculate candies, and then from
+        right to left recalculate candies, then i return the
+        sum of candies array.
+
 
 */
 
 int candy(vector<int>& ratings) {
     int n = ratings.size();
-    vector<int> candies(n, 0);
+    /* Intialize new vector with 1 since every student has to 
+       have at lease one candy */
+    vector<int> candies(n, 1);
 
-    candies[0] = 1;
-
+    /* iterate over ratings every time a ratings[i] found bigger 
+       ratings at i + 1 increment rating[i] with 1 */
     for (int i = 1; i < n; i++) {
         if (ratings[i] > ratings[i - 1])
-            candies[i] = candies[i - 1] + 1;
-        else
-            candies[i] = 1;
+            candies[i]++;
     }
 
+    /* iterate over the ratings reversevely if rating[i] is 
+    greater than ratings[i + 1] and candies[i] are less or 
+    equal to candies[i + 1] the candies[i] should be greater
+    than candies[i + 1] with 1 */
     for (int i = n - 2; i >= 0; i--) {
         if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1])
             candies[i] = candies[i + 1] + 1;
     }
 
+    /* calculate the sum */
     int minSum = 0;
     for(int i = 0; i < candies.size(); i++)
         minSum += candies[i];
